5189ec8c8917b6619e2893b0a4c006effcd034fb
Aditya Pramana <mirror.ojoakua@gmail.com>
2019-12-08 14:57:40 +0700

    Add model for jenjang, mkkelas, periode, rmk

---
+++ apps/modules/SAR/domain/models/Jenjang.php
@@ -0,0 +1,24 @@
+<?php
+
+
+namespace KPL\SAR\Domain\Models;
+
+
+class Jenjang {
+    private $id;
+    private $nama;
+
+    public function __construct($id, $nama) {
+        $this->id = $id;
+        $this->nama = $nama;
+    }
+
+    public function id() {
+        return $this->id;
+    }
+
+    public function nama() {
+        return $this->nama;
+    }
+
+}
---
+++ apps/modules/SAR/domain/models/Rmk.php
@@ -0,0 +1,35 @@
+<?php
+
+
+namespace KPL\SAR\Domain\Models;
+
+
+class Rmk {
+    private $id;
+    private $nama;
+    private $idJurusan;
+    private $idJenjang;
+
+    public function __construct($id, $nama, $idJurusan, $idJenjang) {
+        $this->id = $id;
+        $this->nama = $nama;
+        $this->idJurusan = $idJurusan;
+        $this->idJenjang = $idJenjang;
+    }
+
+    public function id() {
+        return $this->id;
+    }
+
+    public function nama() {
+        return $this->nama;
+    }
+
+    public function idJurusan() {
+        return $this->idJurusan;
+    }
+
+    public function idJenjang() {
+        return $this->idJenjang;
+    }
+}

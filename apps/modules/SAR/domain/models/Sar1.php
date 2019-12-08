f33d161028d40e6dab11d0426b15ef3b39266511
Aditya Pramana <mirror.ojoakua@gmail.com>
2019-12-08 11:34:00 +0700

    Add SAR1 basic domain model

--- apps/modules/SAR/domain/models/Sar1.php
+++ apps/modules/SAR/domain/models/Sar1.php
@@ -4,5 +4,53 @@ namespace KPL\SAR\Domain\Models;
 
 
 class Sar1 {
+    private $id;
+    private $idJenjang;
+    private $idPeriode;
+    private $capaian;
+    private $sasaran;
+    private $pengisi;
+    private $isLocked;
 
+    public function __construct(SarId $id, $idJenjang, $idPeriode, $capaian, $sasaran = 0, $pengisi, $isLocked = false) {
+        $this->id = $id;
+        $this->idJenjang = $idJenjang;
+        $this->idPeriode = $idPeriode;
+        $this->capaian = $capaian;
+        $this->sasaran = $sasaran;
+        $this->pengisi = $pengisi;
+        $this->isLocked = $isLocked;
+    }
+
+    public function id() {
+        return $this->id;
+    }
+
+    public function idJenjang() {
+        return $this->idJenjang;
+    }
+
+    public function idPeriode() {
+        return $this->idPeriode;
+    }
+
+    public function capaian() {
+        return $this->capaian;
+    }
+
+    public function sasaran() {
+        return $this->sasaran;
+    }
+
+    public function pengisi() {
+        return $this->pengisi;
+    }
+
+    public function isLocked() {
+        return $this->isLocked;
+    }
+
+    public function lockSar() {
+        $this->isLocked = true;
+    }
 }

7e36202ccaa6341117fe14ed008c946104ecc461
Muh Isa S <sasenoaji@gmail.com>
2019-12-16 17:13:12 +0700

    fix

--- apps/modules/SAR/controllers/web/Sar1Controller.php
+++ apps/modules/SAR/controllers/web/Sar1Controller.php
@@ -20,21 +20,12 @@ class Sar1Controller extends Controller
         $ServiceSarM = new SarMasterService($SarRepository);
         $ResponseSarM = $ServiceSarM->execute($RequestSarM);
         
-<<<<<<< HEAD
-        $SarAssigment = $ResponseSar->SarAssigment;
+        $SarAssigment = $ResponseSarM->SarAssigment;
         $ListSar = $this->session->get("ListSar");
 
         $this->view->setVar('SarAssigment',$SarAssigment);
         $this->view->setVar('TipeSar',$ListSar);
         $this->view->pick('sar/warek/kelola-sar-1/index');
-=======
-        $Sar1Assigment = $ResponseSarM->SarAssigment;
-        $ListSar = $this->session->get("ListSar");
-        var_dump($Sar1Assigment);
-        //$this->view->setVar('SarAssigment',$Sar1Assigment);
-        //$this->view->setVar('TipeSar',$ListSar);
-        //$this->view->pick('sar/warek/kelolasar/index');
->>>>>>> a6836f0f854374a559e79144ef77eaf6ae1a245c
     }
 
     // public function get

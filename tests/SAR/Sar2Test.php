a3c0ced32a86268ad43932b627ae0cb4a4668f6a
Aditya Pramana <mirror.ojoakua@gmail.com>
2019-12-17 16:06:06 +0700

    Add some testcases

--- tests/SAR/Sar2Test.php
+++ tests/SAR/Sar2Test.php
@@ -1,29 +1,30 @@
 <?php
 declare(strict_types=1);
 
-use KPL\SAR\Domain\Model\Sar1;
+use KPL\SAR\Domain\Model\Sar2;
 use PHPUnit\Framework\TestCase;
 
-class Sar1Test extends TestCase {
+class Sar2Test extends TestCase {
     public function testCanBeInstantiated(): void {
-        $sar1 = new Sar1(1, "S1", "2019/2020 GASAL", 1.0, 1.0, "Foo", true);
+        $sar2 = new Sar2(1, "S1", "2019/2020 GASAL", "FTIK", 1.0, 1.0, "Foo", true);
 
-        $this->assertInstanceOf(Sar1::class, $sar1);
+        $this->assertInstanceOf(Sar2::class, $sar2);
     }
 
     public function testCanBeInstantiatedWithoutOptionalParams(): void {
-        $sar1 = new Sar1(1, "S1", "2019/2020 GASAL", 1.0, 1.0, "Foo");
+        $sar2 = new Sar2(1, "S1", "2019/2020 GASAL", "FTIK", 1.0, 1.0, "Foo");
 
-        $this->assertFalse($sar1->isLocked());
+        $this->assertFalse($sar2->isLocked());
     }
 
     public function testCanGenerateArray(): void {
-        $sar1 = new Sar1(1, "S1", "2019/2020 GASAL", 1.0, 1.0, "Foo", true);
-        $arrayResult = $sar1->generateToArray();
+        $sar2 = new Sar2(1, "S1", "2019/2020 GASAL", "FTIK", 1.0, 1.0, "Foo", true);
+        $arrayResult = $sar2->generateToArray();
 
         $this->assertArrayHasKey("id", $arrayResult);
         $this->assertArrayHasKey("jenjang", $arrayResult);
         $this->assertArrayHasKey("periode", $arrayResult);
+        $this->assertArrayHasKey("fakultas", $arrayResult);
         $this->assertArrayHasKey("sasaran", $arrayResult);
         $this->assertArrayHasKey("capaian", $arrayResult);
         $this->assertArrayHasKey("pengisi", $arrayResult);
